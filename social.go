37de57fa4e48662cb432a467d314b37780187fd9
wuriyanto <wuriyanto48@yahoo.co.id>
2018-09-19 07:39:06 +0700

    finishing social.go

--- social.go
+++ social.go
@@ -1,7 +1,22 @@
 package social
 
+import (
+	"errors"
+
+	"github.com/wuriyanto48/go-social/api"
+	"github.com/wuriyanto48/go-social/internal/facebook"
+	"github.com/wuriyanto48/go-social/internal/github"
+	"github.com/wuriyanto48/go-social/internal/google"
+	"github.com/wuriyanto48/go-social/internal/linkedin"
+)
+
 // Type generic type of social login
 type Type int
+
+// Provider structure
+type Provider struct {
+	Providers map[string]api.Service
+}
 
 const (
 	// Facebook type
@@ -37,4 +52,25 @@ func (t Type) String() string {
 	default:
 		return "Facebook"
 	}
+}
+
+func newProvider(clientID, clientSecret, redirectURI string) *Provider {
+	providers := make(map[string]api.Service)
+	providers["Facebook"] = facebook.New(clientID, clientSecret, redirectURI)
+	providers["Google"] = google.New(clientID, clientSecret, redirectURI)
+	providers["Github"] = github.New(clientID, clientSecret, redirectURI)
+	providers["Linkedin"] = linkedin.New(clientID, clientSecret, redirectURI)
+	return &Provider{providers}
+}
+
+// New , return api.Service implementation
+func New(socialType Type, clientID, clientSecret, redirectURI string) (api.Service, error) {
+	providers := newProvider(clientID, clientSecret, redirectURI)
+	provider, ok := providers.Providers[socialType.String()]
+
+	if !ok {
+		return nil, errors.New("invalid provider type")
+	}
+
+	return provider, nil
 }
--- social_test.go
+++ social_test.go
@@ -2,6 +2,8 @@ package social
 
 import (
 	"testing"
+
+	"github.com/wuriyanto48/go-social/internal/facebook"
 )
 
 func TestSocial(t *testing.T) {
@@ -12,4 +14,33 @@ func TestSocial(t *testing.T) {
 	if googleType.String() != expected {
 		t.Error("Error Social type conversion")
 	}
+}
+
+func TestProviderType(t *testing.T) {
+
+	t.Run("Should return Facebook", func(t *testing.T) {
+		expected1 := "https://graph.facebook.com/oauth/access_token"
+		expected2 := "https://graph.facebook.com/v2/oauth/access_token"
+
+		result, err := New(Facebook, "client_id", "client_secret", "http://localhost:8080/callback")
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		// convert result to facebook
+		actual, _ := result.(*facebook.Facebook)
+
+		if actual.TokenURI != expected1 {
+			t.Error("value is not Facebook")
+		}
+
+		// test mutate
+		actual.TokenURI = "https://graph.facebook.com/v2/oauth/access_token"
+
+		if actual.TokenURI != expected2 {
+			t.Error("value is not Facebook")
+		}
+
+	})
 }
